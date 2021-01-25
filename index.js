ce75b3fab54d0b18c3fd4cb57c5c4331ced32598
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-25 13:39:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 console.log(dodger.style.left); 
+console.log(dodger.style.bottom); 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
