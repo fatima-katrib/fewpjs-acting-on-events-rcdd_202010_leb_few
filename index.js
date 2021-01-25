3f3ffa106fb9c2d3396e08c6fbaa214ac16d03e9
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-25 13:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 39) {
+    moveDodgerRight()}
