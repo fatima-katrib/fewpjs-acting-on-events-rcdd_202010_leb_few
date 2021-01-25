0d355bedf1d444c4e6e7a949b2ee9b6b2dc8c7fa
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-25 13:39:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
-
+console.log(dodger.style.left); 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
